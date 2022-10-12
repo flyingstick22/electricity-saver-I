43f95de9066346e9899bc3afba65109b9321c401
Flying Stick <flyingstick22@gmail.com>
2022-10-12 22:16:13 +0200

    extra comment deleted

--- electricity_saver/day_ahead_prices.py
+++ electricity_saver/day_ahead_prices.py
@@ -28,8 +28,6 @@ def get_prices():
     tz = "Europe/Helsinki"      #time zone
     
     client = EntsoeRawClient("add_your_own_entsoe_api_key_here")   #add your API key between " "   
-  #  client = EntsoeRawClient("your_own_API_key_here")   #add your API key between " "   
-
 
     start_date_yyyymmdd = (datetime.today() + timedelta(days=1)).strftime('%Y%m%d')
     end_date_yyyymmdd = start_date_yyyymmdd
